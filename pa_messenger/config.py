080e45e7db04e91d462b5e32a7892ff12b76d507
chasetawilson <35085072+chasetawilson@users.noreply.github.com>
2018-01-07 21:30:29 -0800

    Update config.py

--- pa_messenger/config.py
+++ pa_messenger/config.py
@@ -5,9 +5,9 @@ class DefaultConfig(object):
     SECRET_KEY = 'youshouldreallychangethis'
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://mews:mews@localhost/mewsdb')
-    TWILIO_ACCOUNT_SID = 'PN7a1dac08e9a95538b91c6b081cb4f2ca'
-    TWILIO_AUTH_TOKEN = 'ed72af786552d4223ad2034a6787f6b5'
-    TWILIO_NUMBER = '+15036943356'
+    TWILIO_ACCOUNT_SID = ''
+    TWILIO_AUTH_TOKEN = ''
+    TWILIO_NUMBER = ''
     BASIC_AUTH_USERNAME = 'admin'
     BASIC_AUTH_PASSWORD = 'SoSecret'
 
